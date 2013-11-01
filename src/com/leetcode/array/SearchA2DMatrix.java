62cf2632dc8d83e70626e9727d8d95dad6688219
HiddenFox <trendhanfeng@gmail.com>
2013-11-01 20:50:18 +0800

    Improve SearchA2DMatrix

--- src/com/leetcode/array/SearchA2DMatrix.java
+++ src/com/leetcode/array/SearchA2DMatrix.java
@@ -23,48 +23,75 @@ package com.leetcode.array;
  */
 public class SearchA2DMatrix {
 
+//	public static boolean searchMatrix(int[][] matrix, int target) {
+//		// IMPORTANT: Please reset any member data you declared, as
+//		// the same Solution instance will be reused for each test case.
+//		if (matrix.length == 0 || matrix[0].length == 0) {
+//			return false;
+//		}
+//
+//		int xMin = 0;
+//		int xMax = matrix[0].length - 1;
+//		int yMin = 0;
+//		int yMax = matrix.length - 1;
+//
+//		int xCurrent = 0, yCurrent = 0;
+//
+//		if (matrix[0][0] > target) {
+//			return false;
+//		} else if (matrix[yMax][0] < target) {
+//			yCurrent = yMax;
+//		} else {
+//			while (yMin < yMax) {
+//				yCurrent = (yMin + yMax) / 2;
+//
+//				if (matrix[yMin][0] == target || matrix[yMax][0] == target || matrix[yCurrent][0] == target) {
+//					return true;
+//				} else if (yMin == yCurrent) {
+//					break;
+//				} else if (matrix[yCurrent][0] > target) {
+//					yMax = yCurrent;
+//				} else {
+//					yMin = yCurrent;
+//				}
+//			}
+//		}
+//
+//		while (xMin <= xMax) {
+//			xCurrent = (xMin + xMax) / 2;
+//			if (matrix[yCurrent][xCurrent] == target) {
+//				return true;
+//			} else if (matrix[yCurrent][xCurrent] > target) {
+//				xMax = xCurrent - 1;
+//			} else {
+//				xMin = xCurrent + 1;
+//			}
+//		}
+//
+//		return false;
+//	}
+
 	public static boolean searchMatrix(int[][] matrix, int target) {
-		// IMPORTANT: Please reset any member data you declared, as
-		// the same Solution instance will be reused for each test case.
+
 		if (matrix.length == 0 || matrix[0].length == 0) {
 			return false;
 		}
 
-		int xMin = 0;
-		int xMax = matrix[0].length - 1;
-		int yMin = 0;
-		int yMax = matrix.length - 1;
+		int xLength = matrix[0].length;
+		int min = 0;
+		int max = matrix[0].length * matrix.length - 1;
 
-		int xCurrent = 0, yCurrent = 0;
-
-		if (matrix[0][0] > target) {
-			return false;
-		} else if (matrix[yMax][0] < target) {
-			yCurrent = yMax;
-		} else {
-			while (yMin < yMax) {
-				yCurrent = (yMin + yMax) / 2;
-
-				if (matrix[yMin][0] == target || matrix[yMax][0] == target || matrix[yCurrent][0] == target) {
-					return true;
-				} else if (yMin == yCurrent) {
-					break;
-				} else if (matrix[yCurrent][0] > target) {
-					yMax = yCurrent;
-				} else {
-					yMin = yCurrent;
-				}
-			}
-		}
-
-		while (xMin <= xMax) {
-			xCurrent = (xMin + xMax) / 2;
-			if (matrix[yCurrent][xCurrent] == target) {
-				return true;
-			} else if (matrix[yCurrent][xCurrent] > target) {
-				xMax = xCurrent - 1;
+		int x, y, current;
+		while (min <= max) {
+			current = (min + max) / 2;
+			y = current / xLength;
+			x = current % xLength;
+			if (matrix[y][x] == target) {
+				return  true;
+			} else if (target < matrix[y][x]) {
+				max = current - 1;
 			} else {
-				xMin = xCurrent + 1;
+				min = current + 1;
 			}
 		}
 
@@ -72,17 +99,17 @@ public class SearchA2DMatrix {
 	}
 
 	public static void main(String args[]) {
-		System.out.println(searchMatrix(new int[][]{{1},{3}}, 0));
-		System.out.println(searchMatrix(new int[][]{{1},{3}}, 1));
-		System.out.println(searchMatrix(new int[][]{{1},{3}}, 3));
-		System.out.println(searchMatrix(new int[][]{{1},{3}}, 4));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, -11));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, -10));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, -7));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 0));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 1));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 6));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 8));
-		System.out.println(searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 9));
+		System.out.println("false " + searchMatrix(new int[][]{{1},{3}}, 0));
+		System.out.println("true " + searchMatrix(new int[][]{{1},{3}}, 1));
+		System.out.println("true " + searchMatrix(new int[][]{{1},{3}}, 3));
+		System.out.println("false " + searchMatrix(new int[][]{{1},{3}}, 4));
+		System.out.println("false " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, -11));
+		System.out.println("true " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, -10));
+		System.out.println("false " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, -7));
+		System.out.println("true " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 0));
+		System.out.println("true " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 1));
+		System.out.println("true " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 6));
+		System.out.println("true " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 8));
+		System.out.println("false " + searchMatrix(new int[][]{{-10,-10},{-9,-9},{-8,-6},{-4,-2},{0,1},{3,3},{5,5},{6,8}}, 9));
 	}
 }
